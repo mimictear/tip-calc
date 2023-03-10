5574b36836a01ca08a7ec0e7ef5c2cdb589a667f
vorontsov <mimictear@outlook.com>
2023-03-10 12:00:50 +0100

    Initial Commit

---
+++ TipCalc/TipCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalcApp.swift
+//  TipCalc
+//
+//  Created by ANDREY VORONTSOV on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TipCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
